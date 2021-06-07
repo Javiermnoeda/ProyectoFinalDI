793d2fcb4e48a7093792eefcaee1cd472997d9a1
Javiermnoeda <jmnoeda2000@gmail.com>
2021-06-07 23:21:32 +0200

    Para Clase

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -14,7 +14,7 @@ public class Controller {
 
     ControllerPlantilla controllerJugadores = null;
 
-    Jugador steph,klay, dbook, cp3, demar, dj, kd, jh, jimmy, bam, kl7, p;
+    Jugador steph,klay, dbook, cp3, jokic, jammal, kd, jh, jimmy, bam, kl7, p;
 
     @FXML
     ImageView warriors,suns,spurs,nets,heat,raptors;
@@ -32,8 +32,8 @@ public class Controller {
         klay = new Jugador("Klay Thompson",false,"GSW","Escolta",11);
         dbook = new Jugador("Devin Booker",false,"PHO","Escolta",1);
         cp3 = new Jugador("Chris Paul",false,"PHO","Base",3);
-        demar = new Jugador("Demar Derozan",false,"SAS","Escolta",10);
-        dj = new Jugador("Dejonte Murray",false,"SAS","Escolta",6);
+        jokic = new Jugador("Nikola Jokic",false,"DEN","Pivot",15);
+        jammal = new Jugador("Jammal Murray",false,"DEN","Base",27);
         kd = new Jugador("Kevin Durant",false,"BKN","Alero",7);
         jh = new Jugador("James Harden",false,"BKN","Base",13);
         jimmy = new Jugador("Jimmy Butler",false,"MIA","Alero",22);
@@ -45,8 +45,8 @@ public class Controller {
         listaWarriors.add(klay);
         listaSuns.add(dbook);
         listaSuns.add(cp3);
-        listaSpurs.add(demar);
-        listaSpurs.add(dj);
+        listaSpurs.add(jokic);
+        listaSpurs.add(jammal);
         listaNets.add(kd);
         listaNets.add(jh);
         listaHeat.add(bam);
@@ -80,10 +80,10 @@ public class Controller {
             controllerJugadores.imagenEquipo.setImage(imagenEquipo);
 
 
-            Image imagenKlay = new Image("sample/resources/klay.PNG");
-            Image imagenCurry = new Image("sample/resources/curry.PNG");
-            controllerJugadores.foto1.setImage(imagenCurry);
-            controllerJugadores.foto2.setImage(imagenKlay);
+            Image imagenJ2 = new Image("sample/resources/klay.PNG");
+            Image imagenJ1 = new Image("sample/resources/curry.PNG");
+            controllerJugadores.foto1.setImage(imagenJ1);
+            controllerJugadores.foto2.setImage(imagenJ2);
 
 
             controllerJugadores.nombreEquipo.setText(steph.equipo);
@@ -112,6 +112,98 @@ public class Controller {
             controllerJugadores.nombreEquipo.setText(kd.equipo);
             controllerJugadores.nombreJ1.setText(kd.nombre);
             controllerJugadores.nombreJ2.setText(jh.nombre);
+        } catch (Exception e){
+            System.out.println("No se que pasa");
+        }
+    }
+
+    @FXML
+    public void pulsandoHeat(){
+        try{
+            abrirVentanaJugadores();
+
+            Image imagenEquipo = new Image("sample/resources/heat.PNG");
+            controllerJugadores.imagenEquipo.setImage(imagenEquipo);
+
+
+            Image imagenJ1 = new Image("sample/resources/jimmy.PNG");
+            Image imagenJ2 = new Image("sample/resources/bam.PNG");
+            controllerJugadores.foto1.setImage(imagenJ1);
+            controllerJugadores.foto2.setImage(imagenJ2);
+
+
+            controllerJugadores.nombreEquipo.setText(jimmy.equipo);
+            controllerJugadores.nombreJ1.setText(jimmy.nombre);
+            controllerJugadores.nombreJ2.setText(bam.nombre);
+        } catch (Exception e){
+            System.out.println("No se que pasa");
+        }
+    }
+
+    @FXML
+    public void pulsandoSuns(){
+        try{
+            abrirVentanaJugadores();
+
+            Image imagenEquipo = new Image("sample/resources/suns.PNG");
+            controllerJugadores.imagenEquipo.setImage(imagenEquipo);
+
+
+            Image imagenJ1 = new Image("sample/resources/dbook.PNG");
+            Image imagenJ2 = new Image("sample/resources/cp3.PNG");
+            controllerJugadores.foto1.setImage(imagenJ1);
+            controllerJugadores.foto2.setImage(imagenJ2);
+
+
+            controllerJugadores.nombreEquipo.setText(dbook.equipo);
+            controllerJugadores.nombreJ1.setText(dbook.nombre);
+            controllerJugadores.nombreJ2.setText(cp3.nombre);
+        } catch (Exception e){
+            System.out.println("No se que pasa");
+        }
+    }
+
+    @FXML
+    public void pulsandoNuggets(){
+        try{
+            abrirVentanaJugadores();
+
+            Image imagenEquipo = new Image("sample/resources/denver.PNG");
+            controllerJugadores.imagenEquipo.setImage(imagenEquipo);
+
+
+            Image imagenJ1 = new Image("sample/resources/jokic.PNG");
+            Image imagenJ2 = new Image("sample/resources/jammal.PNG");
+            controllerJugadores.foto1.setImage(imagenJ1);
+            controllerJugadores.foto2.setImage(imagenJ2);
+
+
+            controllerJugadores.nombreEquipo.setText(jokic.equipo);
+            controllerJugadores.nombreJ1.setText(jokic.nombre);
+            controllerJugadores.nombreJ2.setText(jammal.nombre);
+        } catch (Exception e){
+            System.out.println("No se que pasa");
+        }
+    }
+
+    @FXML
+    public void pulsandoRaptors(){
+        try{
+            abrirVentanaJugadores();
+
+            Image imagenEquipo = new Image("sample/resources/raptors.PNG");
+            controllerJugadores.imagenEquipo.setImage(imagenEquipo);
+
+
+            Image imagenJ1 = new Image("sample/resources/kl7.PNG");
+            Image imagenJ2 = new Image("sample/resources/pascal.PNG");
+            controllerJugadores.foto1.setImage(imagenJ1);
+            controllerJugadores.foto2.setImage(imagenJ2);
+
+
+            controllerJugadores.nombreEquipo.setText(kl7.equipo);
+            controllerJugadores.nombreJ1.setText(kl7.nombre);
+            controllerJugadores.nombreJ2.setText(p.nombre);
         } catch (Exception e){
             System.out.println("No se que pasa");
         }
